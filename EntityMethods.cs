b854815cd24dd0eb183705a8799beaddcaf129e3
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2025-03-31 15:19:01 +0200

    Re-implemented Controller actions

--- EntityMethods.cs
+++ EntityMethods.cs
@@ -6,91 +6,73 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ECFramework;
 
-public partial class EntityController<E> : CSDFrameworkPMSPatch.CSDController where E : Entity, new()
+public partial class EntityController<E> : CSDFrameworkPMSPatch.CSDController where E : class, new()
 {
     [NonAction]
-    public T GetItem<T>(T item) where T : Entity, new()
-    {
-        // Get the DbSet instead of IQueryable
-        DbSet<T> dbSet = ctx.Set<T>();
-
-        var keys = item.GetKeys();
-        if (keys.Length != 0)
-        {
-            if (Guid.TryParse(keys[0].ToString(), out Guid firstKey) && firstKey == Guid.Empty)
-                return new T();
-            else
-                return dbSet.Find(keys);  // Use Find on DbSet for composite keys
-        }
-
-        return null;
-    }
-
-    [NonAction]
-    public IQueryable<T> Where<T>(Expression<Func<T, bool>> predicate) where T : Entity, new()
+    public IQueryable<T> Where<T>(Expression<Func<T, bool>> predicate) where T : class, new()
     {
         DbSet<T> dbSet = ctx.Set<T>();
         return dbSet.Where(predicate);
     }
 
     [NonAction]
-    public IQueryable<TResult> Select<T, TResult>(Expression<Func<T, TResult>> selector) where T : Entity, new()
+    public IQueryable<TResult> Select<T, TResult>(Expression<Func<T, TResult>> selector) where T : class, new()
     {
         DbSet<T> dbSet = ctx.Set<T>();
         return dbSet.Select(selector);
     }
 
     [NonAction]
-    public IQueryable<T> OrderBy<T, TKey>(Expression<Func<T, TKey>> keySelector) where T : Entity, new()
+    public IQueryable<T> OrderBy<T, TKey>(Expression<Func<T, TKey>> keySelector) where T : class, new()
     {
         DbSet<T> dbSet = ctx.Set<T>();
         return dbSet.OrderBy(keySelector);
     }
 
     [NonAction]
-    public IQueryable<T> OrderByDescending<T, TKey>(Expression<Func<T, TKey>> keySelector) where T : Entity, new()
+    public IQueryable<T> OrderByDescending<T, TKey>(Expression<Func<T, TKey>> keySelector) where T : class, new()
     {
         DbSet<T> dbSet = ctx.Set<T>();
         return dbSet.OrderByDescending(keySelector);
     }
 
     [NonAction]
-    public IQueryable<IGrouping<TKey, T>> GroupBy<T, TKey>(Expression<Func<T, TKey>> keySelector) where T : Entity, new()
+    public IQueryable<IGrouping<TKey, T>> GroupBy<T, TKey>(Expression<Func<T, TKey>> keySelector) where T : class, new()
     {
         DbSet<T> dbSet = ctx.Set<T>();
         return dbSet.GroupBy(keySelector);
     }
 
     [NonAction]
-    public T FirstOrDefault<T>(Expression<Func<T, bool>> predicate) where T : Entity, new()
+    public T FirstOrDefault<T>(Expression<Func<T, bool>> predicate) where T : class, new()
     {
         DbSet<T> dbSet = ctx.Set<T>();
         return dbSet.FirstOrDefault(predicate);
     }
 
     [NonAction]
-    public IQueryable<T> Skip<T>(int count) where T : Entity, new()
+    public IQueryable<T> Skip<T>(int count) where T : class, new()
     {
         DbSet<T> dbSet = ctx.Set<T>();
         return dbSet.Skip(count);
     }
 
     [NonAction]
-    public IQueryable<T> Take<T>(int count) where T : Entity, new()
+    public IQueryable<T> Take<T>(int count) where T : class, new()
     {
         DbSet<T> dbSet = ctx.Set<T>();
         return dbSet.Take(count);
     }
 
     [NonAction]
-    public IQueryable<T> ToList<T>() where T : Entity, new()
+    public IQueryable<T> ToList<T>() where T : class, new()
     {
         DbSet<T> dbSet = ctx.Set<T>();
         return dbSet.ToList().AsQueryable();
     }
 
     [NonAction]
-    public T Add<T>(T item) where T : Entity, new()
+    public T Add<T>(T item) where T : class, new()
     {
         ctx.Set<T>().Add(item);
         ctx.SaveChanges();
@@ -98,12 +80,12 @@ public partial class EntityController<E> : CSDFrameworkPMSPatch.CSDController wh
     }
 
     [NonAction]
-    public T Update<T>(T item) where T : Entity, new()
+    public T Update<T>(T item) where T : class, new()
     {
         IQueryable<T> query = ctx.Set<T>();
         DbSet<T> dbSet = (DbSet<T>)query;
 
-        var res = dbSet.Find(item.GetKeys());
+        var res = dbSet.Find(item);
         ctx.Entry(item).CurrentValues.SetValues(item);
         ctx.SaveChanges();
 
@@ -111,13 +93,13 @@ public partial class EntityController<E> : CSDFrameworkPMSPatch.CSDController wh
     }
 
     [NonAction]
-    public bool Remove<T>(T req) where T : Entity, new()
+    public bool Remove<T>(T req) where T : class, new()
     {
         var res = new Response<T>(); //FIX: Throw error
         IQueryable<T> query = ctx.Set<T>();
         DbSet<T> dbSet = (DbSet<T>)query;
 
-        res.item = dbSet.Find(req.GetKeys());
+        res.item = dbSet.Find(req);
 
         ctx.Set<T>().Remove(res.item);
         ctx.SaveChanges();
