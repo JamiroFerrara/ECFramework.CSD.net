5055e6b100122e43337136a9035f0ff13ed1d471
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2025-03-31 14:05:42 +0200

    Removed useless comment

--- Errors.cs
+++ Errors.cs
@@ -7,7 +7,6 @@ using CSD.Framework.NetCore.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECFramework;
-//Update test
 
 public partial class EntityController<E> : CSDFrameworkPMSPatch.CSDController where E : Entity, new()
 {
