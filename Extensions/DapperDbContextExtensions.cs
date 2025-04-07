5ecbda4ddf2f072061fcf8fd44e2ad82f565a343
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2025-04-07 15:25:45 +0200

    Longer timeout for dapper

--- Extensions/DapperDbContextExtensions.cs
+++ Extensions/DapperDbContextExtensions.cs
@@ -42,7 +42,7 @@ public static class DapperDbContextExtensions
     )
     {
         var connection = context.Database.GetDbConnection();
-        return parameters == null ? Dapper.SqlMapper.Query(connection, text) : Dapper.SqlMapper.Query(connection, text, parameters);
+        return parameters == null ? Dapper.SqlMapper.Query(connection, text) : Dapper.SqlMapper.Query(connection, text, parameters, null, true, 300);
     }
 
     //Explicit Type
