6d7cbad4e64d6e0a26864117dbf1e87fdaf6dad9
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2025-04-08 17:08:55 +0200

    SingalR Upload progress websocket implementation

--- S3/S3EntityController.cs
+++ S3/S3EntityController.cs
@@ -1,28 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Amazon.S3;
-using Amazon.S3.Model;
+using Amazon.S3.Transfer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Builder;
+
+//NOTE: SignalR Upload Progress hub + Extension -> app.AddUploadHub()
+public class UploadProgressHub : Hub { public async Task SendProgress(string connectionId, string message) => await Clients.Client(connectionId).SendAsync("ReceiveProgress", message); }
+public static class UploadHubExtensions { public static void AddUploadHub(this IApplicationBuilder app) { app.UseEndpoints(endpoints => { endpoints.MapHub<UploadProgressHub>("/uploadProgress"); }); } }
 
 public partial class EntityController<E> : Controller where E : class, new()
 {
     public IAmazonS3 s3Client;
+    private readonly IHubContext<UploadProgressHub> _hubContext;
+    private IHubContext<UploadProgressHub> hubContext;
 
-    public EntityController(IAmazonS3 s3Client, DbContext ctx, IConfiguration configuration)
+    public EntityController(IHubContext<UploadProgressHub> hubContext, IAmazonS3 s3Client, DbContext ctx, IConfiguration configuration)
     {
         this.s3Client = s3Client;
         this.ctx = ctx;
+        this.hubContext = hubContext;
     }
 
     [HttpPost("Upload")]
-    public virtual async Task<Response<E>> _Upload([FromForm] E item) => await Upload(item, query => query);
+    public virtual async Task<Response<E>> _Upload([FromForm] E item, string connectionId) => await Upload(item, connectionId, query => query);
 
     [NonAction]
-    public virtual async Task<Response<E>> Upload(E item, Func<DbSet<E>, DbSet<E>> action)
+    public virtual async Task<Response<E>> Upload(E item, string connectionId, Func<DbSet<E>, DbSet<E>> action)
     {
         //BUG: Context disappears in Try(() => ) for some fucking reason
 
@@ -46,18 +51,26 @@ public partial class EntityController<E> : Controller where E : class, new()
 
         using (var stream = s3Item.file.OpenReadStream())
         {
-            var putRequest = new PutObjectRequest
+            stream.Position = 0; // Ensure correct read position
+
+            var fileTransferUtility = new TransferUtility(s3Client);
+            var putRequest = new TransferUtilityUploadRequest
             {
                 BucketName = S3Configuration.BucketName,
                 Key = key,
                 InputStream = stream,
-                ContentType = s3Item.MimeType, // Ensure proper content type
-                AutoResetStreamPosition = true, // Important to prevent SHA-256 mismatch
-                UseChunkEncoding = false, // Disable chunked transfer encoding for S3-compatible providers
+                ContentType = s3Item.MimeType,
+                DisableDefaultChecksumValidation = true //NOTE: This is disabled, not needed for putRequest
             };
 
-            await s3Client.PutObjectAsync(putRequest);
+            putRequest.UploadProgressEvent += async (s, e) =>
+            {
+                await this.hubContext.Clients.Client(connectionId).SendAsync("ReceiveProgress", (int)((e.TransferredBytes / (double)e.TotalBytes) * 100));
+            };
+
+            await fileTransferUtility.UploadAsync(putRequest);
         }
+
         query.Add(item);
 
         if (item is IModifiable modifiable)
