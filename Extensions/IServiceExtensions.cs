0d05ac4e6e9907abcd5247d8d0c6e333fac19210
Jamiro Ferrara <j.ferrara@novigo-consulting.it>
2026-07-06 11:37:35 +0200

    PreSigned URL injection

--- Extensions/IServiceExtensions.cs
+++ Extensions/IServiceExtensions.cs
@@ -10,14 +10,43 @@ public static class IServiceExtensions
         Injectables.Create<IModifiable>((item, context) => item.ModDate = DateTime.Now);
         Injectables.Create<ICreateable>((item, context) => item.CreatedAt = DateTime.Now);
 
-        //TODO: Mabye this should be called n times
         Injectables.GetPage(async (item, context) =>
         {
             IAmazonS3 client = context.s3Client;
             foreach (var x in item)
             {
-                if (x is IS3Object s3Object)
-                    s3Object.url = await client.GeneratePreSignedURLAsync(s3Object.Id, s3Object.Name);
+                if (x is IS3Object obj)
+                    obj.url = await client.GeneratePreSignedURLAsync(obj.Id, obj.Name);
+
+                // Use reflection to iterate over all properties of the SAMPLE_PACK object
+                var properties = x.GetType().GetProperties();
+                foreach (var property in properties)
+                {
+                    // Check if the property is of type IS3Object
+                    if (typeof(IS3Object).IsAssignableFrom(property.PropertyType))
+                    {
+                        // Handle single IS3Object
+                        var s3Object = property.GetValue(x) as IS3Object;
+                        if (s3Object != null)
+                            s3Object.url = await client.GeneratePreSignedURLAsync(s3Object.Id, s3Object.Name);
+                    }
+                    // Check if the property is a collection of IS3Object
+                    else if (property.PropertyType.IsGenericType &&
+                             typeof(IS3Object).IsAssignableFrom(property.PropertyType.GenericTypeArguments[0]))
+                    {
+                        // Handle collection of IS3Object
+                        var collection = property.GetValue(x) as System.Collections.IEnumerable;
+                        if (collection != null)
+                        {
+                            foreach (var itemInCollection in collection)
+                            {
+                                var s3Object = itemInCollection as IS3Object;
+                                if (s3Object != null)
+                                    s3Object.url = await client.GeneratePreSignedURLAsync(s3Object.Id, s3Object.Name);
+                            }
+                        }
+                    }
+                }
             }
         });
 
